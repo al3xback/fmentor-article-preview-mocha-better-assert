1f71305256da0e1a3e62a5e22dff07c37f597bdc
alex <al3xback@gmail.com>
2024-01-08 14:38:09 +0700

    added first commit

---
+++ test/main.js
@@ -0,0 +1,61 @@
+import assert from 'better-assert';
+import jsdom from 'jsdom';
+import got from 'got';
+
+const { JSDOM } = jsdom;
+
+const url = 'https://al3xback.github.io/fmentor-article-preview-flex/';
+
+const getData = () => {
+	return got(url)
+		.then((res) => {
+			const { document } = new JSDOM(res.body).window;
+			return document;
+		})
+		.catch((err) => {
+			throw new Error(err);
+		});
+};
+
+describe('DOM', () => {
+	beforeEach(async () => {
+		try {
+			const document = await getData();
+			global.document = document;
+
+			const isAnImageExist = (name) => {
+				if (!name) {
+					return;
+				}
+
+				const imageElements = document.querySelectorAll('img');
+
+				let isImageExist = false;
+
+				for (let i = 0; i < imageElements.length; i++) {
+					const source = imageElements[i].src;
+
+					if (source.includes(`${name}.jpg`)) {
+						isImageExist = true;
+						break;
+					}
+				}
+
+				return isImageExist;
+			};
+			global.isAnImageExist = isAnImageExist;
+		} catch (err) {
+			console.log(err);
+		}
+	});
+
+	it("should have a 'drawers' image with jpg extention inside the card element", () => {
+		const isImageExist = isAnImageExist('drawers');
+		assert(isImageExist);
+	});
+
+	it("should have a 'avatar' image with jpg extention inside the card element", () => {
+		const isImageExist = isAnImageExist('avatar');
+		assert(isImageExist);
+	});
+});
